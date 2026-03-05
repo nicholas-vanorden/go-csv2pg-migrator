0b115c63c93302e5d2664f9bd949304c10ff9950
Nick Van Orden <nicholas.vanorden83@gmail.com>
2026-03-05 14:10:55 -0700

    Add stubbed out transform functions

--- internal/transform/registry.go
+++ internal/transform/registry.go
@@ -6,3 +6,13 @@ var Registry = map[string]TransformFunc{
 	"oracle_date": OracleDate,
 	"oracle_bool": OracleBool,
 }
+
+func OracleDate(input string) (any, error) {
+	// Implement your date transformation logic here
+	return input, nil
+}
+
+func OracleBool(input string) (any, error) {
+	// Implement your boolean transformation logic here
+	return input, nil
+}
